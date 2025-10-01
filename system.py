8687240ab1cbabde78292ec01385b06a5c616fe2
Jakob Olason <jakobolason@proton.me>
2025-10-01 10:14:32 +0200

    det virker

--- system.py
+++ system.py
@@ -24,9 +24,9 @@ class System:
         print(f"killed node {node_id}!")
 
     def revive_node(self, node_id: int):
-        node_to_kill = self.nodes[node_id-1]
-        node_to_kill.kill_node()
-        print(f"killed node {node_id}!")
+        node_to_revive = self.nodes[node_id-1]
+        node_to_revive.revive_node()
+        print(f"reviveded node {node_id}!")
 
     def node_to_ping_leader(self, node_id: int):
         """
@@ -45,27 +45,34 @@ class System:
             print(f"{node.node_id}: {state} Believed leader: {node.leader_id}")
         print("----------------------------------")
 
+    def addNewNode(self):
+        node_id = len(self.nodes) + 1
+        my_node = Node(node_id, list(range(1, node_id)))
+        my_node.start_node()
+        self.nodes.append(my_node)
+        print(f"added node {node_id}")
+
 
 if __name__ == '__main__':
-    system = System(20)
+    system = System(18)
     print('made nodes :)')
     # while True:
     system.getSystemSummary()
-    time.sleep(10)
-    system.kill_node(20)
-    system.kill_node(19)
+    time.sleep(7)
+    system.kill_node(17)
     system.kill_node(18)
     system.kill_node(16)
     system.kill_node(11)
-    system.revive_node(20)
     time.sleep(1)
     system.node_to_ping_leader(8)
     system.getSystemSummary()
-    
-    # Add new node
-     my_node = Node(node_id, known_nodes)
-    my_node.start_node()
-    self.nodes.append(my_node)
-    known_nodes.append(node_id)
+    system.revive_node(18)
+    system.getSystemSummary()
+    system.revive_node(16)
+    system.getSystemSummary()
 
-    
+    # Add new node
+    time.sleep(1)
+    system.addNewNode()
+    system.getSystemSummary()
+
